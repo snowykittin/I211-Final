75f19223fd3cc17a6ace814f89ee8518a08ffa45
Summer S <snowykittin@gmail.com>
2023-04-03 11:43:32 -0400

    updates to home

--- views/home/home_index.class.php
+++ views/home/home_index.class.php
@@ -3,7 +3,14 @@
 class HomeIndex extends IndexView{
     public function display(){
         parent::displayHeader("Infinibank");
+        ?>
+        <div class="hero-banner">
+            <div class="banner-text">
+                Banking in the New Age
+            </div>
+        </div>
 
+        <?php
         parent::displayFooter();
     }
 
--- views/index_view.class.php
+++ views/index_view.class.php
@@ -12,18 +12,21 @@ class IndexView
             <meta charset="UTF-8">
             <title><?php echo $page_title ?></title>
             <link type='text/css' rel='stylesheet' href='<?= BASE_URL ?>/www/css/styles.css' />
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400&display=swap" rel="stylesheet">
         </head>
         <body>
             <div class="header">
                 <div class="title-bar">
                     <!-- LOGO AND INFO HERE -->
-                    <a href="<?= BASE_URL ?>/index.php"><img src="<?= BASE_URL ?>/www/images/infinibank-transparent.png"></a>
+                    <a href="<?= BASE_URL ?>/index.php"><img src="<?= BASE_URL ?>/www/images/infinibank-1.png"></a>
                 </div>
                 <nav>
 <!--                    ADD NAVIGATION -->
-                    <a href="<?= BASE_URL ?>/index.php">HOME</a>
-                    <a href="<?= BASE_URL ?>/index.php">ABOUT</a>
-                    <a href="<?= BASE_URL ?>/index.php">LOG IN</a>
+                    <a href="<?= BASE_URL ?>/index.php">Why Us?</a> |
+                    <a href="<?= BASE_URL ?>/index.php">Contact</a> |
+                    <a href="<?= BASE_URL ?>/index.php">Log In</a>
                 </nav>
             </div>
             <div class="content">
