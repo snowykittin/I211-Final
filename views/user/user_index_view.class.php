c70fcfaf672de966a364311eef425b2d15e44823
JFlor <Jfloreancig@gmail.com>
2023-04-13 12:42:53 -0400

    Commit Test

--- views/user/user_index_view.class.php
+++ views/user/user_index_view.class.php
@@ -13,16 +13,15 @@ class UserIndexView extends IndexView
         </script>
         <nav>
             <!--                    ADD NAVIGATION -->
-            <a href="<?= BASE_URL ?>/user/login">LOGIN</a>
-            <a href="<?= BASE_URL ?>/user/register">REGISTER</a>
-            <a href="<?= BASE_URL ?>/user/edit">EDIT</a>
-            <a href="<?= BASE_URL ?>/user/delete">DELETE</a>
+            <a href="<?= BASE_URL ?>/user/login" style="color:black;">LOGIN</a>
+            <a href="<?= BASE_URL ?>/user/register" style="color:black;">REGISTER</a>
+            <a href="<?= BASE_URL ?>/user/edit" style="color:black;">EDIT</a>
+            <a href="<?= BASE_URL ?>/user/delete" style="color:black;">DELETE</a>
 
 
         </nav>
 
         <?php
-        echo "Please display SOMETHING";
     }
 
     public static function displayFooter()
